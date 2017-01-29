d5ca7d3b015dfff4316868bb1a1d4853e8af4fc8
createjump <joeypence88@gmail.com>
2017-01-28 19:41:22 -0500

    Add inital things that were already here

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+/*			*\
+
+This file is part of Playback.
+
+Any modifications to this 
+file must be recnogized by
+the person(s) who did such.
+
+Copyright (c) 2017 createjump & Spectre.
+
+\*			*/
+// main.cpp: The application's entry point.
+
+
+
+
+#include <iostream> // Input and output (*shudder* why must opening and closing a file be so hard?)
+#include <stdio.h> // C Standard libary IO library
+
+int main(int argc, char *argv[]){
+std::cout << "IT WORKS!" << std::endl;
+std::cout << argc << " arguments passed to me, arg0 is program name." << std::endl;
+
+return 0;
+}
+
+
