8b28eea49a0df4714e0771f77f495bd8053391b1
createjump <joeypence88@gmail.com>
2017-02-02 08:43:56 -0500

    add logging stuff (standard io)

--- src/devcons.cpp
+++ src/devcons.cpp
@@ -7,13 +7,27 @@ Copyright (c) 2017 createjump & Spectre.
 \*			                   */
 
 #include <iostream>
-
+#include <stdlib.h>
 
 typedef int CVAR_INT;
 typedef std::string CVAR_STR;
 typedef bool CVAR_BOOL;
 
 namespace playback{
-
+  
+namespace Logger{
+void Log(std::string info){
+cout << "[LOG] " << info << endl; 
+}
+void Warning(std::string info){
+cout << "[WARN] " << info << endl; 
+}
+void Assert(std::string info){
+cout << "[ERROR] " << info << endl; 
+exit(1);
+}
+  
+}
+  
 
 }
