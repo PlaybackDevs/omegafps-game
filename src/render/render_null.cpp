d21493b99f9aa77fcd3a94edfdb26dfa533371c6
createjump <joeypence88@gmail.com>
2017-02-04 17:09:04 -0500

    add some form of render backend
    Now that most of the codebase is mature enough, I can now add the beginnings of a renderer so that we can add different backends (ie Vulkan "if we wanted to, lol") but we will be focusing on OpenGL because it's the most mature, that and the only other popular Vulkan game is DOOM 2016.

---
+++ src/render/render_null.cpp
@@ -0,0 +1,23 @@
+/*			                   *\
+
+This file is part of Playback version 0.1 Alpha.
+
+Copyright (c) 2017 createjump & Spectre.
+
+\*			                   */
+
+
+// A null renderer
+
+#include "util.h"
+
+
+namespace playback{
+
+namespace render{
+void RenderInit(){
+Log("Null render init", __FILE__, __LINE__);
+}
+}
+
+}
---
+++ src/render/render_null.h
@@ -0,0 +1,19 @@
+/*			                   *\
+
+This file is part of Playback version 0.1 Alpha.
+
+Copyright (c) 2017 createjump & Spectre.
+
+\*			                   */
+
+
+// A null renderer
+
+
+namespace playback{
+
+namespace render{
+void RenderInit();
+}
+
+}
