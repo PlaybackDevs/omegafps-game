f746a50a3483c69ca69cddea59678e173600100b
createjump <joeypence88@gmail.com>
2017-02-07 13:25:13 -0500

    add log so ghpages rebuilds into docs folder

--- src/Player.cpp
+++ src/Player.cpp
@@ -1,9 +1,9 @@
 #include "Player.h"
-
+#include "util.h"
 Player::Player(unsigned int id, std::string name)
 {
     // put code here for player init
-    // lol hi guys
+    playback::logger::Log("new player","player", __FILE__, __LINE__);
 }
 
 Player::~Player()
