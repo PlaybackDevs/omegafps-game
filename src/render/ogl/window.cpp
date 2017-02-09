3decc7e2133835a14b9ce87631b399ff8ee19f20
createjump <joeypence88@gmail.com>
2017-02-09 10:48:54 -0500

    Window class beginnings

---
+++ src/render/ogl/window.cpp
@@ -0,0 +1,20 @@
+// temp comment until i go home
+
+
+class Window{
+private:
+int m_WndXsiz;
+int m_wndYsiz
+public:
+
+int GetXsize(){
+return m_WndXsiz;
+}
+
+int GetYsize(){
+return m_WndYsiz;
+}
+
+
+
+}
