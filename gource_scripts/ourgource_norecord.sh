298f1acd1b20583a13c4f5066064a74e9506f7a7
createjump <joeypence88@gmail.com>
2017-02-21 19:12:50 -0500

    edit 2

--- gource_scripts/ourgource_norecord.sh
+++
@@ -1,2 +0,0 @@
-# Run Gource 
-gource --user-image-dir ./devs_images/ -1280x720 -font-size 24 -seconds-per-day 5 -key -date-format "%A, %b %d %I:%M%p %Z" -title "Playback Progress as of $(date +%x)"
